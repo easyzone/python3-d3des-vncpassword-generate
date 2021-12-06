bff0d215a99e7dfd9321090cea01b9841af246d1
EasyZoneCorpDotNet <easyhorpak@gmail.com>
2021-12-06 14:16:47 +0700

    Create generate_vnc_password.py

---
+++ generate_vnc_password.py
@@ -0,0 +1,13 @@
+from d3des import * 
+
+def vnc_encode(password):
+    passpadd = (password + '\x00'*8)[:8]
+    strkey = ''.join([ chr(x) for x in vnckey ])
+    ekey = deskey(bytearray(strkey, encoding="ascii"), False)
+    return desfunc(bytearray(passpadd, encoding="ascii"), ekey)
+
+
+pwd = vnc_encode('123456')
+
+#pwd.hex is work for tightvncsevrer for windows
+print(pwd.hex())
